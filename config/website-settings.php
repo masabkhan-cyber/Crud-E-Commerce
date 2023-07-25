f294c31a5eeb29ae06750b1edbf9d3297779a4f7
masabkhan-cyber <75547252+masabkhan-cyber@users.noreply.github.com>
2023-07-25 20:04:56 +0500

    Ignore
    Ignore

--- config/website-settings.php
+++ config/website-settings.php
@@ -1,8 +1,8 @@
 <?php
 
 return array (
-  'website_name' => 'MASABTech',
+  'website_name' => 'MASAB.Tech',
   'phone_number' => '+92-317-33-555-34',
-  'email' => 'saleemmasab@gmail.com',
+  'email' => 'YourEmail@XYZ.Com',
   'logo_path' => 'public/logos/default_logo.png',
 );
