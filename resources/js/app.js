6a8b43f4ed9e6ca830f0457a0e23e058c4dc86cb
masabkhan-cyber <75547252+masabkhan-cyber@users.noreply.github.com>
2024-06-28 22:49:56 +0500

    prod version
    Datatables prod version

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,13 @@
+// Importing necessary modules
 import './bootstrap';
 import $ from 'jquery';
-window.$ = window.jQuery = $;
-
 import 'datatables.net';
 import 'datatables.net-bs4';
 
+// Assigning jQuery to global window object
+window.$ = window.jQuery = $;
+
+// Initializing DataTables when the DOM is fully loaded
 $(function() {
     $('#product-table').DataTable();
 });
-
-
-require('jquery');
-require('datatables.net');
-require('datatables.net-bs4');
